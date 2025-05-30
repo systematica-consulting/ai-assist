e72083d1e26f66bf5295fe5b670b82b766578b9b
alexei <alexei.maidanik@gmail.com>
2025-05-30 14:55:34 +0300

    add config

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ import logging
 
 # Telegram Bot Token
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "здесь ваш токен телеграм бота")
-
+AI_TOKEN = os.environ.get("AI_TOKEN", "здесь ваш токен телеграм бота")
+DEEP_SEEK_TOKEN = os.environ.get("DEEP_SEEK_TOKEN", "здесь ваш токен телеграм бота")
