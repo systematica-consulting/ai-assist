9c5fecd9784bc660b32617d577c5c5149402f684
alexei <alexei.maidanik@gmail.com>
2025-06-02 17:24:28 +0300

    aa

---
+++ migrations/migrations/aa.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY,
+  tg_id INTeger unique,
+  url VARCHAR(2048) ,
+  login VARCHAR(2048) ,
+  "password" VARCHAR(2048) ,
+  tgname VARCHAR(2048)
+
+);
+CREATE TABLE history(
+    ID SERIAL primary key,
+    user_id integer,
+    foreign key (user_id) REFERENCES users(ID),
+    is_old bool default false not null,
+    request text,
+    response text
+);
+
+
+
+DROP TABLE IF EXISTS history CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
