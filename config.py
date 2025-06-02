0646d3bfa9096a194234719850b6d920ba478017
alexei <alexei.maidanik@gmail.com>
2025-06-02 12:49:28 +0300

    add conn to db

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ import logging
 
 
 # Telegram Bot Token
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "здесь ваш токен телеграм бота")
-AI_TOKEN = os.environ.get("AI_TOKEN", "здесь ваш токен телеграм бота")
-DEEP_SEEK_TOKEN = os.environ.get("DEEP_SEEK_TOKEN", "здесь ваш токен телеграм бота")
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "Ваш токен")
+AI_TOKEN = os.environ.get("AI_TOKEN", "Ваш токен")
+DEEP_SEEK_TOKEN = os.environ.get("DEEP_SEEK_TOKEN", "Ваш токен")
+db_name= os.environ.get("DB_NAME", "Ваш токен")
+db_user= os.environ.get("DB_USER", "Ваш токен")
+db_password= os.environ.get("DB_PASSWORD", "Ваш токен")
+db_host= os.environ.get("DB_HOST", "Ваш токен")
